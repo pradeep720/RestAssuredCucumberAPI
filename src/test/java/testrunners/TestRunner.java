0e964bac2b1cae3106228eed2c20a49b60f7f55f
Pradeep S <pradeep.s@ctlep.local>
2025-05-06 20:15:50 +0530

    one file changed

--- src/test/java/testrunners/TestRunner.java
+++ src/test/java/testrunners/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
     features = "src/test/resources/features/Example.feature",
     glue = {"stepdefinations"},
-    plugin = {"pretty", "html:target/cucumber-reports.html"},
+    plugin = {"pretty", "html:target/cucumber-report/cucumber-reports.html", "json:target/cucumber-report/Cucumber.json","rerun:target/rerun.txt" },
     monochrome = true
 
 )
