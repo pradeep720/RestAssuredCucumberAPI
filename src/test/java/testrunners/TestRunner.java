81b2060444fd533a6a7be997a8c7ae5b7e5560dd
pradeep s <73422862+pradeep720@users.noreply.github.com>
2025-05-19 12:27:39 +0530

    Update TestRunner.java

--- src/test/java/testrunners/TestRunner.java
+++ src/test/java/testrunners/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "C:\\Users\\pradeep.s\\eclipse-workspace\\cucumber\\src\\test\\resources\\features\\Example.feature",
+    features = "src/test/resources/features/Example.feature",
     glue = {"stepdefinations"},
     plugin = {"pretty", "html:target/cucumber-reports.html"},
     monochrome = true
