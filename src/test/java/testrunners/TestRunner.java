580663255935bc67a6d60996dd608598f010c549
pradeep s <73422862+pradeep720@users.noreply.github.com>
2025-06-05 16:44:14 +0530

    Update TestRunner.java

--- src/test/java/testrunners/TestRunner.java
+++ src/test/java/testrunners/TestRunner.java
@@ -8,7 +8,11 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
     features = "src/test/resources/features/Example.feature",
     glue = {"stepdefinations"},
-    plugin = {"pretty", "json:target/cucumber/cucumber.json"},
+    plugin = {
+       "pretty", 
+       "json:target/cucumber/cucumber.json",
+       "html:target/cucumber/html-report"
+},
     monochrome = true
 
 )
